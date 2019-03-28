7f9b8063b6b742406423bc6af1a6aba4644fe523
camila.gusmao <camila.gusmao@radixeng.com.br>
2019-03-28 13:29:59 -0300

    Criando listener para o botão salvar do formulário

--- app/src/main/java/com/example/agenda/FormularioActivity.java
+++ app/src/main/java/com/example/agenda/FormularioActivity.java
@@ -2,14 +2,26 @@ package com.example.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FormularioActivity extends AppCompatActivity {
 
-    //TODO: Salvar dados preenchidos independente da orientação da tela.
+    //TODO: Salvar dados preenchidos independente de mudança de orientação da tela.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        Button btSalvar = findViewById(R.id.btSalvar);
+
+        btSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormularioActivity.this,"Botão clicado!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
