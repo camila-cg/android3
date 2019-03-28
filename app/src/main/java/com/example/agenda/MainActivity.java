81d377ef5f34b3dbb0c5eb0c85f7f284a6684756
camila.gusmao <camila.gusmao@radixeng.com.br>
2019-03-28 12:41:17 -0300

    Criando ListView mockando dados

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] pessoas = {"01", "02", "06"};
+        ListView listaPessoas = findViewById(R.id.lista_pessoas); //TODO: Adicionar ButterKnife
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, pessoas);
+        listaPessoas.setAdapter(adapter);
+
     }
 }
