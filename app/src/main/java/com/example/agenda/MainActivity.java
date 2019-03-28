07cc79545320fe04d9be4ccfe0be8da9d831249c
camila.gusmao <camila.gusmao@radixeng.com.br>
2019-03-28 15:25:21 -0300

    Criando intent para navegar para o formulário através do floating button e finalizando activity do form ao clicar em salvar

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +18,15 @@ public class MainActivity extends AppCompatActivity {
         ListView listaPessoas = findViewById(R.id.lista_pessoas); //TODO: Adicionar ButterKnife
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, pessoas);
         listaPessoas.setAdapter(adapter);
+
+        Button btCadastrar = findViewById(R.id.btAdicionarCadastro);
+        btCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FormularioActivity.class );
+                startActivity(intent);
+            }
+        });
 
     }
 }
