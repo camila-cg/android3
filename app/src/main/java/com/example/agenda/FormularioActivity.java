0c71f29c3ebc2d856aff70fcc336cdcbb616d83f
camila.gusmao <camila.gusmao@radixeng.com.br>
2019-03-28 13:13:25 -0300

    Adicionando scroll no formulário

--- app/src/main/java/com/example/agenda/FormularioActivity.java
+++ app/src/main/java/com/example/agenda/FormularioActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class FormularioActivity extends AppCompatActivity {
 
+    //TODO: Salvar dados preenchidos independente da orientação da tela.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
