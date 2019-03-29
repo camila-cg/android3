cc5d28ec0087d069291589ef957ca93a882b9ea8
camila.gusmao <camila.gusmao@radixeng.com.br>
2019-03-29 09:41:12 -0300

    Criando método de pesquisa de pessoas

--- app/src/main/java/com/example/agenda/dao/PessoaDAO.java
+++ app/src/main/java/com/example/agenda/dao/PessoaDAO.java
@@ -2,10 +2,14 @@ package com.example.agenda.dao;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.example.agenda.modelo.Pessoa;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PessoaDAO extends SQLiteOpenHelper {
 
@@ -45,7 +49,7 @@ public class PessoaDAO extends SQLiteOpenHelper {
     }
 
 
-    public void cadastraPessoa(Pessoa pessoa) {
+    public void cadastrarPessoa(Pessoa pessoa) {
         SQLiteDatabase db = getWritableDatabase();
         ContentValues dados = new ContentValues(); //funciona como um map do java (chave e valor)
 
@@ -57,5 +61,32 @@ public class PessoaDAO extends SQLiteOpenHelper {
 
         db.insert("Pessoa", null, dados);
 
+    }
+
+    /** Efetua a busca por Pessoas no banco.
+     * Como o resultado da execução da query não retorna uma lista de objetos do tipo Pessoa, precisamos manipular o resultado com cursor.
+     * O cursor aponta para um linha em branco exatamente acima dos resultados, por isso é importante usar o moveToNext() para mover o cursor e validar se há mais registros.
+     * @return lista de pessoas cadastradas.
+     */
+    public List<Pessoa> pesquisarPessoas() {
+        String sql = "SELECT * FROM Pessoa;";
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery(sql, null);
+
+        List<Pessoa> pessoas = new ArrayList<Pessoa>();
+        while(cursor.moveToNext()){
+            Pessoa pessoa = new Pessoa();
+            pessoa.setId(cursor.getLong(cursor.getColumnIndex("id")));
+            pessoa.setNome(cursor.getString(cursor.getColumnIndex("nome")));
+            pessoa.setEndereco(cursor.getString(cursor.getColumnIndex("endereco")));
+            pessoa.setTelefone(cursor.getString(cursor.getColumnIndex("telefone")));
+            pessoa.setSite(cursor.getString(cursor.getColumnIndex("site")));
+            pessoa.setNota(cursor.getDouble(cursor.getColumnIndex("nota")));
+
+            pessoas.add(pessoa);
+        }
+        cursor.close();
+
+        return pessoas;
     }
 }
