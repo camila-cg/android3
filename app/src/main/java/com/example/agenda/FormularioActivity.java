1a2e3e9e05b879a685d0c9334380fafa53516a0b
camila.gusmao <camila.gusmao@radixeng.com.br>
2019-04-01 12:13:46 -0300

    Adicionando ação de abrir câmera (not working yet)

--- app/src/main/java/com/example/agenda/FormularioActivity.java
+++ app/src/main/java/com/example/agenda/FormularioActivity.java
@@ -1,15 +1,22 @@
 package com.example.agenda;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.agenda.dao.PessoaDAO;
 import com.example.agenda.modelo.Pessoa;
+
+import java.io.File;
+import java.util.Arrays;
 
 public class FormularioActivity extends AppCompatActivity {
     private FormularioHelper helper;
@@ -22,12 +29,31 @@ public class FormularioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_formulario);
         helper = new FormularioHelper(this);
 
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
         Pessoa pessoa = (Pessoa) intent.getSerializableExtra("pessoa");
         if(pessoa != null){
             //Abro o formulário com os dados de um registro existente para fins de edição.
             helper.preencheFormulario(pessoa);
         }
+
+        //Botão de tirar foto para cadastro
+        //cada aplicação só pode escrever na área q foi instalada
+        Button btnFoto = findViewById(R.id.btn_camera);
+        btnFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //String caminhoFoto = getExternalFilesDirs(null) + "/foto.jpg"; //passamos null pq n queremos uma pasta específica, mas sim a pasta raíz da aplicação.
+                String caminhoFoto = Arrays.toString(getExternalFilesDirs(null)) + "/" + System.currentTimeMillis() + ".jpg";
+                File arquivoFoto = new File(caminhoFoto);
+                intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(arquivoFoto));
+
+                //TODO: ESTÁ CRASHANDO AO TENTAR ABRIR A CAMERA -> aula 2 - vídeo 2
+                startActivity(intentCamera);
+
+            }
+        });
 
     }
 
